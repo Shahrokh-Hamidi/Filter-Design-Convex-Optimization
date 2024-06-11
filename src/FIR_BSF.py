713978c03e4ffb17cf5bc19dee2582e7b8316f56
Shahrokh Hamidi <156338354+Shahrokh-Hamidi@users.noreply.github.com>
2024-06-10 23:32:11 -0400

    Update FIR_BSF.py

--- src/FIR_BSF.py
+++ src/FIR_BSF.py
@@ -1,6 +1,10 @@
 # Created by Shahrokh Hamidi
 # PhD., Electrical & Computer Engineering, University of Waterloo
 # Waterloo, ON., Canada
+# shahrokh.hamidi@uwaterloo.ca
+# shahrokh.hamidi@gmail.com
+
+
 
 
 
